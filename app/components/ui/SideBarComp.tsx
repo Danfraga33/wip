230150de47214bc85255bb81c1e4a821bfa47219
Daniel Fraga <dan.fraga33@gmail.com>
2024-10-18 12:14:36 +0100

    feat: add sidebarcomp

--- app/components/ui/SideBarComp.tsx
+++ app/components/ui/SideBarComp.tsx
@@ -11,7 +11,7 @@ import { motion } from 'framer-motion';
 import { cn } from '~/lib/utils';
 import { Link } from '@remix-run/react';
 
-export function SidebarDemo() {
+export function SidebarComponent() {
 	const links = [
 		{
 			label: 'Logout',
@@ -25,11 +25,11 @@ export function SidebarDemo() {
 	return (
 		<div
 			className={cn(
-				'rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 max-w-7xl mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden',
+				'rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-screen flex-1 max-   w-screen-9xl mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden',
 				'h-screen' // for your use case, use `h-screen` instead of `h-[60vh]`
 			)}
 		>
-			<Sidebar open={open} setOpen={setOpen} animate={false}>
+			<Sidebar open={open} setOpen={setOpen} animate={true}>
 				<SidebarBody className="justify-between gap-10">
 					<div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
 						<>
@@ -41,6 +41,7 @@ export function SidebarDemo() {
 							))}
 						</div>
 					</div>
+
 					<div>
 						<SidebarLink
 							link={{
@@ -76,7 +77,7 @@ export const Logo = () => {
 				animate={{ opacity: 1 }}
 				className="font-medium text-black dark:text-white whitespace-pre"
 			>
-				Acet Labs
+				Automation
 			</motion.span>
 		</Link>
 	);
@@ -94,15 +95,34 @@ export const LogoIcon = () => {
 
 // Dummy dashboard component with content
 const Dashboard = () => {
+	const statInfo = [
+		{
+			title: 'QR Codes Scanned',
+			description: 'lorem',
+		},
+		{
+			title: 'QR Codes redeemed',
+			description: 'lorem',
+		},
+		{
+			title: 'performanceRatio',
+			description: 'lorem',
+		},
+
+		{ title: 'revenue', description: 'lorem' },
+		{ title: 'revenueGrowth', description: 'lorem' },
+	];
 	return (
 		<div className="flex flex-1">
 			<div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
 				<div className="flex gap-2">
-					{[...new Array(4)].map((i) => (
+					{statInfo.map((dataPoint, i) => (
 						<div
 							key={'first' + i}
 							className="h-20 w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
-						></div>
+						>
+							{dataPoint.title}
+						</div>
 					))}
 				</div>
 				<div className="flex gap-2 flex-1">
