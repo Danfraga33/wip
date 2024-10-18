87f84c27723f7b8d556393542cab7e8b1b596c4e
Daniel Fraga <dan.fraga33@gmail.com>
2024-10-18 12:17:06 +0100

    feat: add sidebar component

---
+++ app/routes/dashboard/route.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SidebarComponent } from '~/components/ui/SideBarComp';
+
+const Dashboard = () => {
+	return (
+		<div className="flex h-screen items-center justify-center w-screen">
+			<SidebarComponent />
+		</div>
+	);
+};
+
+export default Dashboard;
